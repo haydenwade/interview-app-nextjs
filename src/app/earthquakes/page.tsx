0e562e1a0441c899365de2fd27d879a537a2ac23
Hayden Wade <htwade51@gmail.com>
2024-06-27 13:29:24 -0600

    Added example route and page for earthquakes

---
+++ src/app/earthquakes/page.tsx
@@ -0,0 +1,9 @@
+import { Stack, Typography } from "@mui/material";
+
+export default function EarthQuakesPage() {
+  return (
+    <Stack>
+      <Typography variant="h1">Earthquakes</Typography>
+    </Stack>
+  );
+}
